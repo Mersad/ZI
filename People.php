1f18dcd3314d1faf9af46873de4013b2f42e9984
Mersad Ajanović <mehoaj@hotmail.com>
2016-09-04 17:30:53 +0200

    Initial commit

---
+++ People.php
@@ -0,0 +1,21 @@
+<?php
+
+class People
+{
+	private $height;
+	private $weight;
+	private $age;
+	private $sex;
+	
+	function __construct() {
+		
+	}
+	
+	function getAge() {
+		if ($this->sex === 'W') {
+			return "You don't ask a lady about her age! How rude!";
+		} else {
+			return $this->sex;
+		}
+	}
+}
